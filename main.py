46dae6be912703f1c1e01b30e7b6497dc9f9e750
humanbeing-dev <maciej.sitowski@gmail.com>
2021-10-31 11:49:52 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def hello_world():
+    return {"Hello": "World"}
+
+
+
+@app.get("/component/{component_id}")
+async def get_component(component_id: int):
+    return {"component_id": component_id}
+
+
+@app.get("/component/")
+async def read_component(number: int, text: Optional[str]):
+    return {"number": number, "text": text}
